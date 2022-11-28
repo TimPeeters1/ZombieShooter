1ce982ba135675071a097d76367231aac4dc17dc
Tim Peeters <tim.peeters12345@gmail.com>
2022-11-28 17:09:45 +0100

    Removed Unused Plugins, Changed Config files to support builds for 5.1

--- Plugins/AsyncBlueprintsExtension/Source/AsyncBlueprintsExtension/Private/AsyncUtility.cpp
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2022 Semyon Gritsenko
-
-#include "AsyncUtility.h"
-
-
--- Plugins/AsyncBlueprintsExtension/Source/AsyncBlueprintsExtension/Private/Tasks/ShortAsyncTask.cpp
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2022 Semyon Gritsenko
-
-#include "Tasks/ShortAsyncTask.h"
-
-EAsyncExecution UShortAsyncTask::getAsyncExecutionType() const
-{
-	return EAsyncExecution::ThreadPool;
-}
